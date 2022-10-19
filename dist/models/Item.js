6840112772244e9e755b3bf97ea43f829efec259
Tevissaur <berserkboar@protonmail.com>
2022-10-19 11:55:57 -0400

    fixing gitignore

--- dist/models/Item.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const ItemSchema = new mongoose_1.Schema({
-    name: String
-});
-const Item = (0, mongoose_1.model)('Item', ItemSchema);
-exports.default = Item;
